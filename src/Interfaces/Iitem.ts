eb61c7d1c73ba2cb68341336db05456c7d091a5d
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2018-09-30 22:56:25 -0500

    Add item more detail data

--- src/Interfaces/Iitem.ts
+++ src/Interfaces/Iitem.ts
@@ -2,6 +2,13 @@ export interface Iitem {
     name: String,
     urlSprite: String,
     idDex: Number,
-    effect: String,
-    category: String
+    effect_entries: {},
+    held_by_pokemon: {},
+    category: String,
+    cost: Number,
+    fling: {},
+    machines: {},
+    baby_trigger_for: {},
+    evolution_for: {},
+    attributes: {}
 }
--- src/models/Schema/Item.ts
+++ src/models/Schema/Item.ts
@@ -8,8 +8,15 @@ export const ItemSchema: Schema = new Schema({
     name: String,
     urlSprite: String,
     idDex: Number,
-    effect: String,
-    category: String
+    effect_entries: [Array],
+    held_by_pokemon: [Array],
+    category: String,
+    cost: Number,
+    fling: [Array],
+    machines: [Array],
+    baby_trigger_for: [Array],
+    evolution_for: [Array],
+    attributes: [Array],
 }, { timestamps: true, versionKey: false })
 
 export const Item: Model<ItemModel> = model<ItemModel>('item', ItemSchema)
