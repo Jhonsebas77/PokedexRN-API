e0aa6bfb788f19e91ada16d41de6e77aa8827b7e
Sebastian Otalora <sebastian.otarola@almundo.com>
2019-03-17 10:54:07 -0500

    Add more atributes to item

--- src/database/schemas/Item_list/index.ts
+++ src/database/schemas/Item_list/index.ts
@@ -5,6 +5,17 @@ export const ItemListSchema: Schema = new Schema({
     url: String,
     urlSprite: String,
     idDex: Number,
-    type: String
+    type: String,
+    effect_entries: {
+        effect: String
+    },
+    held_by_pokemon: [Array],
+    category: String,
+    cost: Number,
+    fling: [Array],
+    machines: [Array],
+    baby_trigger_for: [Array],
+    evolution_for: [Array],
+    attributes: [Array]
 }, { timestamps: true, versionKey: false })
 export const Item_list: Model<ItemListModel> = model<ItemListModel>('item_list', ItemListSchema)
