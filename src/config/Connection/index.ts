e1c145efd6a1f3a195429177870d6788ed2ac2c1
Sebastian Otalora <jhonsebas77@outlook.com>
2021-02-02 19:53:30 -0500

    Fix DataBase connection

--- src/config/Connection/index.ts
+++ src/config/Connection/index.ts
@@ -1,35 +1,22 @@
-import mongoose, { mongo } from 'mongoose'
+const MongoClient = require('mongodb').MongoClient;
 import bunyan from 'bunyan'
 import config from 'config'
-mongoose.Promise = global.Promise
+MongoClient.Promise = global.Promise
 
 const { user, pwd, db } = config.get('server.database')
-const dbUri = `mongodb://${user}:${pwd}${db}`
+const dbUri = `mongodb+srv://${user}:${pwd}@${db}?retryWrites=true&w=majority`
 const log = bunyan.createLogger({ name: 'Mongo Status:' })
 
 export class Database {
     public static connect(user: string, pwd: string, db: string) {
         try {
-            mongoose.connect(`${dbUri}`, { useNewUrlParser: true })
+            MongoClient.connect(`${dbUri}`, { useNewUrlParser: true })
         } catch (error) {
-            mongoose.createConnection(`${dbUri}`)
+            MongoClient.createConnection(`${dbUri}`)
         }
-        mongoose.connection
+        MongoClient.connection
             .on('error', () => log.info(` => Error en la conexion`))
             .on('disconnected', () => log.info(` => Se desconecto`))
             .once('open', () => log.info(` => Se establecio la conexion`))
     }
-    public static debug(debug: any) {
-        mongoose.set('debug', debug)
-    }
-    // const { user, pwd, db } = config.get('server.database')
-    // const dbUri = `mongodb://${user}:${pwd}${db}`
-    // const log = bunyan.createLogger({ name: 'Mongo Status:' })
-    // mongoose.connect(dbUri)
-    // export const Connect = () => {
-    //     mongoose.connection
-    //         .on('error', () => log.info(` => Error en la conexion`))
-    //         .on('disconnected', () => log.info(` => Se desconecto`))
-    //         .once('open', () => log.info(` => Se establecio la conexion`))
-    // }
 }
--- src/database/connection/index.ts
+++ src/database/connection/index.ts
@@ -1,15 +1,18 @@
-import mongoose from 'mongoose'
-mongoose.Promise = global.Promise
+const mongoose = require('mongoose')
+
 export class Database {
     public static connect(user: string, password: string, db: string) {
-        try {
-            mongoose.connect(`mongodb://${user}:${password}@${db}`, { useNewUrlParser: true })
-        } catch (error) {
-            mongoose.createConnection(`mongodb://${user}:${password}@${db}`)
-        }
+        const url = `mongodb://${user}:${password}@${db}?ssl=true&replicaSet=atlas-11n5sw-shard-0&authSource=admin&retryWrites=true&w=majority`;
+        mongoose.connect(url, { useNewUrlParser: true })
+            .then(() => {
+                console.log('Connected to database ')
+            })
+            .catch((err: any) => {
+                console.error(`Error connecting to the database. \n${err}`);
+            })
         mongoose.connection
             .once('open', () => console.log('MongoDB connection success'))
-            .on('error', (e) => console.error(e))
+            .on('error', (err: any) => console.error(err))
     }
     public static debug(debug: any) {
         mongoose.set('debug', debug)
